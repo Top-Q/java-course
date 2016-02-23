7007fd19ec738ab3c64a7e29c8202d9a9ff7c8da
itaiag <itai.agmon@gmail.com>
2016-02-23 16:31:25 +0200

    WIP

--- JavaCourse/src/objectOriented/Bottle.java
+++ JavaCourse/src/objectOriented/Bottle.java
@@ -2,27 +2,79 @@ package objectOriented;
 
 public class Bottle {
 
-	String material;
-	int height;
-	double capacity = 1.5;
-	String color = "White";
-	double amount = 0;
+	private String material;
+	private int height;
+	private double maxCapacity = 1.5;
+	private String color = "White";
+	private double amountInLiters = 0;
 
-	void fillWithLiquied(double amountInLiters) {
-		if (amount + amountInLiters > capacity){
-			amount = capacity;
+	Bottle() {
+	}
+
+	Bottle(double maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+	
+	Bottle(double maxCapacity, String color) {
+		this.maxCapacity = maxCapacity;
+		this.color = color;
+	}
+
+
+	void fillWithLiquid(double amountInLitersToFill) {
+		if (amountInLiters + amountInLitersToFill > maxCapacity) {
+			amountInLiters = maxCapacity;
 		} else {
-			amount += amountInLiters;
+			amountInLiters += amountInLitersToFill;
 		}
 	}
 
 	void breakBottle() {
 
 	}
-	
-	void empty(){
-		
+
+	void empty() {
+
 	}
-	
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public double getMaxCapacity() {
+		return maxCapacity;
+	}
+
+	public void setMaxCapacity(double maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getAmountInLiters() {
+		return amountInLiters;
+	}
+
+	public void setAmountInLiters(double amountInLiters) {
+		this.amountInLiters = amountInLiters;
+	}
 
 }
--- JavaCourse/src/objectOriented/MyBottleApplication.java
+++ JavaCourse/src/objectOriented/MyBottleApplication.java
@@ -3,14 +3,15 @@ package objectOriented;
 public class MyBottleApplication {
 
 	public static void main(String[] args) {
-		Bottle myBottle = new Bottle();
+		Bottle myBottle = new Bottle(1.5);
 
-		myBottle.fillWithLiquied(1.0);
-		myBottle.fillWithLiquied(1.0);
-		System.out.println(myBottle.amount);
+		myBottle.fillWithLiquid(1.0);
+		myBottle.fillWithLiquid(1.0);
+		System.out.println(myBottle.getAmountInLiters());
 
+		
 		Bottle anotherBottle = new Bottle();
-		System.out.println(anotherBottle.amount);
+		System.out.println(anotherBottle.getAmountInLiters());
 
 	}
 
