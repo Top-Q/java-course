376d70553ec862acd792dba0e242fbda5cdc0b9d
itaiag <itai.agmon@gmail.com>
2016-02-24 12:09:49 +0200

    WIP

--- JavaCourse/src/objectOriented/exercise/EBookReader.java
+++ JavaCourse/src/objectOriented/exercise/EBookReader.java
@@ -2,16 +2,23 @@ package objectOriented.exercise;
 
 public class EBookReader {
 
-	private EBook[] books = new EBook[10];
-
-	private int lastBookIndex;
+	private EBook[] books = new EBook[100];
 
 	public void addBook(EBook book) {
-		books[lastBookIndex++] = book;
+		for (int i = 0 ; i < books.length ; i++){
+			if (books[i] == null){
+				books[i] = book;
+				return;
+			}
+		}
+		System.err.println("No storage left for books");
 	}
 
 	public EBook getBookByTitle(String title) {
 		for (EBook book : books) {
+			if (book == null){
+				continue;
+			}
 			if (book.getTitle().equals(title)) {
 				return book;
 			}
